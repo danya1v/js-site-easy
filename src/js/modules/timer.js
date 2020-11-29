8614fd093f76ba9eca07ced2eb915524e7108b2b
danya <d4nya1v@gmail.com>
2020-11-30 00:00:18 +0300

    work

--- src/js/modules/timer.js
+++ src/js/modules/timer.js
@@ -1,7 +1,56 @@
 const timer = (id, deadline) => {
+   const addZero = (num) => {
+      if(num <= 9){
+         return "0" + num;
+      } else {
+         return num;
+      }
+   };
    const getTimeRemaning = (endtime) => {
-      const t = Data.parse(endTime) - 
-   }
-}
+      const t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60 ),
+            minutes = Math.floor((t / 1000/60) % 60 ),
+            hours = Math.floor((t / (1000*60*60)) % 24 ),
+            days = Math.floor((t / (1000*60*60*24)));
+            return {
+               'total': t,
+               'days' : days,
+               'hours' : hours,
+               'minutes': minutes,
+               'seconds': seconds
+            };
+   };
+
+   const setClock = (selector, endtime) => {
+      const timer = document.querySelector(selector),
+            days = timer.querySelector("#days"),
+            hours = timer.querySelector("#hours"),
+            minutes = timer.querySelector("#minutes"),
+            seconds = timer.querySelector("#seconds"),
+            timeInterval = setInterval(updateClock, 1000);
+
+            function updateClock() {
+               const t = getTimeRemaning(endtime);
+
+               days.textContent = addZero(t.days);
+               hours.textContent = addZero(t.hours);
+               minutes.textContent = addZero(t.minutes);
+               seconds.textContent = addZero(t.seconds);
+
+               if (t.total <= 0) {
+                  days.textContent = '0' ;
+                  hours.textContent = '0' ;
+                  minutes.textContent =  '0' ;
+                  seconds.textContent = '0';
+
+                  clearInterval(timeInterval);
+               }
+            }
+
+   };
+
+   setClock( id, deadline);
+
+};
 
 export default timer
